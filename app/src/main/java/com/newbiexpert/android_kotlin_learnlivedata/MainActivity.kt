50f908c7936e7d5a5f25a19192faf8c723b6f7fd
nando <muhammad.rezki.ananda@gmail.com>
2021-04-27 00:04:21 +0700

    View Model Factory

--- app/src/main/java/com/newbiexpert/android_kotlin_learnlivedata/MainActivity.kt
+++ app/src/main/java/com/newbiexpert/android_kotlin_learnlivedata/MainActivity.kt
@@ -2,10 +2,25 @@ package com.newbiexpert.android_kotlin_learnlivedata
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.newbiexpert.android_kotlin_learnlivedata.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainActivityViewModel
+    private lateinit var viewModelFactory: MainActivityViewModelFactory
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        viewModelFactory = MainActivityViewModelFactory(225)
+        viewModel = ViewModelProvider(this, viewModelFactory).get(MainActivityViewModel::class.java)
+        binding.resultTextView.text = viewModel.getTotal().toString()
+
+        binding.insertButton.setOnClickListener {
+            viewModel.setTotal(binding.inputEditText.text.toString().toInt())
+            binding.resultTextView.text = viewModel.getTotal().toString()
+        }
     }
 }
---
+++ app/src/main/java/com/newbiexpert/android_kotlin_learnlivedata/MainActivityViewModel.kt
@@ -0,0 +1,19 @@
+package com.newbiexpert.android_kotlin_learnlivedata
+
+import androidx.lifecycle.ViewModel
+
+class MainActivityViewModel(startingTotal: Int) : ViewModel() {
+    private var total: Int = 0
+
+    init {
+        total = startingTotal
+    }
+
+    fun getTotal(): Int {
+        return total
+    }
+
+    fun setTotal(input: Int) {
+        total += input
+    }
+}
