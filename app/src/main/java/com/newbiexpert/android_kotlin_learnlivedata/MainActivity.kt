2bac6d5beaab929f9554bd0d091c81f2b77d3b0a
nando <muhammad.rezki.ananda@gmail.com>
2021-05-04 23:12:16 +0700

    View Model With Data Binding

--- app/src/main/java/com/newbiexpert/android_kotlin_learnlivedata/MainActivity.kt
+++ app/src/main/java/com/newbiexpert/android_kotlin_learnlivedata/MainActivity.kt
@@ -15,13 +15,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+        binding.myViewmodel = viewModel
 
         viewModel.count.observe(this, Observer {
             binding.countText.text = it.toString()
         })
 
-        binding.button.setOnClickListener {
-            viewModel.updateCount()
-        }
     }
 }
